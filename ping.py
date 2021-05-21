9150235e38b5048d6882a61b437955dc6278f811
Abhinav <abhinavaashish012@gmail.com>
2021-05-21 18:25:10 +0530

    Formatted the message

--- ping.py
+++ ping.py
@@ -25,7 +25,14 @@ try:
                     for j in range(0,len(d['centers'][i]['sessions'])):
                         #print(f"{d['centers'][i]['sessions'][j]}")   #working display
                         if d['centers'][i]['sessions'][j]['min_age_limit'] ==18 and d['centers'][i]['sessions'][j]['available_capacity_dose1'] >0:
-                            m=f"{d['centers'][i]['name']}======={d['centers'][i]['sessions'][j]['vaccine']}\n\n Age :{d['centers'][i]['sessions'][j]['min_age_limit']} \n\nDate : {date} \n\n Link :\n https://selfregistration.cowin.gov.in"
+                            vaccine =d['centers'][i]['sessions'][j]['vaccine']
+                            center=d['centers'][i]['name']
+                            date_vacc=date
+                            dose1=d['centers'][i]['sessions'][j]['available_capacity_dose1']
+                            dose2=d['centers'][i]['sessions'][j]['available_capacity_dose2']
+                            Link = " https://selfregistration.cowin.gov.in"
+                            m= f"Vaccine : {vaccine} \n\nCenter : {center} \n\nDate : {date_vacc}\n\n Dose1 : {dose1} \n\n Dose2 : {dose2} \n\n Link : {Link}\n\n"
+                            #m=f"{d['centers'][i]['name']}======={d['centers'][i]['sessions'][j]['vaccine']}\n\n Age :{d['centers'][i]['sessions'][j]['min_age_limit']} \n\nDate : {date} \n\n Link :\n https://selfregistration.cowin.gov.in"
                             telegram_msg=f"https://api.telegram.org/bot{chat_id}/sendMessage?chat_id=-524951835&text={m}"
                             print(m)
                             requests.get(telegram_msg)
