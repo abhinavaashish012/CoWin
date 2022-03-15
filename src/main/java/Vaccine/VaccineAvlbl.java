240255ecf963b6e7aef3b49abea2577380c88f9a
Abhinav Aashish <abhinavaashish012@gmail.com>
2022-03-15 22:56:05 +0530

    Sending to Telegram basic details

--- src/main/java/Vaccine/VaccineAvlbl.java
+++ src/main/java/Vaccine/VaccineAvlbl.java
@@ -1,6 +1,7 @@
 package Vaccine;
 
 import com.google.gson.Gson;
+import okhttp3.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -36,33 +37,51 @@ public class VaccineAvlbl {
         String ans = response.toString();
         Gson gson = new Gson();
         CenterList centerList = gson.fromJson(ans, CenterList.class);
-        System.out.println("-----------------------------------------");
-        System.out.println("-----------------------------------------");
-        System.out.println("-----------------------------------------");
         String city ="";
         String state ="";
         long vaccinesInCity = 0;
+        String msg="";
+
         for (Centers c : centerList.getCenters())
         {
             city=c.getDistrict_name();
             state=c.getState_name();
             Sessions[] temp = c.getSessions();
             String vaccine = temp[0].getVaccine();
-            System.out.println(c.getCenter_id()+" "+c.getName()+" "+vaccine);
             long total=0;
             for(Sessions s : temp)
             {
                 total+=s.getTotalVaccines();
             }
             vaccinesInCity+=total;
-            System.out.println("Total vaccines  : "+ total);
-            System.out.println(".......................................");
+            msg = c.getCenter_id()+"%0A%0A"+c.getName()+"%0A%0A"+vaccine+"%0A%0A"+"Total vaccines  : "+ total+"%0A%0A";
+            sendOnTelegram(msg);
         }
 
         System.out.println("*****************************************************************");
-        System.out.println("Total Vaccines in " +city+"/"+state +" is : "+ vaccinesInCity);
+        String finalMsg ="Total Vaccines in " +city+"/"+state +" is : "+ vaccinesInCity;
+        sendOnTelegram(finalMsg);
+        System.out.println(finalMsg);
         System.out.println("*****************************************************************");
+    }
+
+    private static void sendOnTelegram(String msg) throws Exception {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://api.telegram.org/bot1763777741:AAEEh_SSjdSe0s61Rtl8KTJfPi5O62yNKVs/sendMessage?chat_id=-524951835&text="+msg)
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println("Message sent....");
     }
 
 
 }
+/*
+For multiline message in telegram use
+"%0A" instead of "\n"
+https://stackoverflow.com/questions/50018785/telegram-rest-api-send-newline-in-message-text
+*/
